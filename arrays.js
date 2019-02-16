bf18808798bad2dd091cf8ad7b77898f1e4bc0e8
Joe Tustin <j.tustin@gmail.com>
2019-02-16 19:37:54 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -24,3 +24,10 @@ function destructivelyAddElementToEndOfArray(array, element) {
 function accessElementInArray(array, index) {
   return array[index];
 }
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift;
+  return array;
+}
+
+
