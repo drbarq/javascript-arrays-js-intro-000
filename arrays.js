04685b78fb29493b6059a511961ced55c5b11750
Joe Tustin <j.tustin@gmail.com>
2019-02-16 18:47:45 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,7 +3,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element ) {
   array.upshift(element); {
-    return array 
+    return array
   }
 }
 
