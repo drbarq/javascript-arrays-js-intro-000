92c053619f94479b0722f1c46839187046724b76
Joe Tustin <j.tustin@gmail.com>
2019-02-16 19:50:12 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -36,6 +36,7 @@ function removeElementFromBeginningOfArray(array) {
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
-  array.pop;
-  return array;
+  array.pop; {
+    return array;
+  }
 }
