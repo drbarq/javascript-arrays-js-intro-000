300384fc0fdfb8792c5d98fc62cbb40883d112f2
Joe Tustin <j.tustin@gmail.com>
2019-02-16 19:39:17 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -26,7 +26,8 @@ function accessElementInArray(array, index) {
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  array.shift(); {
-    return array;
-  }
+  array.shift(); 
+  return array;
 }
+
+
