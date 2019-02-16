1288137edd06f432563cf4e20498afde98e96dc0
Joe Tustin <j.tustin@gmail.com>
2019-02-16 18:43:05 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -7,7 +7,7 @@ var ingredient6 = "garlic"
 
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-function addElementToBeginningOfArray = 
+function addElementToBeginningOfArray =
 
 
-function destructivelyAddElementToBeginningOfArray = 
+function destructivelyAddElementToBeginningOfArray =
